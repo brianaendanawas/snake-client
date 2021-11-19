2fae6787b261c0922822c10053606a5c969bcd61
Briana Endanawas <brianamay48@gmail.com>
2021-11-19 18:34:06 -0500

    exit program

--- play.js
+++ play.js
@@ -1,4 +1,24 @@
 const { connect } = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+}; 
+
+const standardInput = setupInput();
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.stdout.write('Exit program');
+    process.exit();
+  }
+}
+
+standardInput.on("data", handleUserInput);
+
 console.log("Connecting ...");
 connect();
+//setupInput(); 
