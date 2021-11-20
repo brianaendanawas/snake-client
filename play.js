2b0a4457fd741939ee2f750fa521d97618759cf0
Briana Endanawas <brianamay48@gmail.com>
2021-11-20 07:24:27 -0500

    moved functions to input.js

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
 const { connect } = require('./client');
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-}; 
-
-const standardInput = setupInput();
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.stdout.write('Exit program');
-    process.exit();
-  }
-}
-
-standardInput.on("data", handleUserInput);
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
-//setupInput(); 
+
+setupInput();
