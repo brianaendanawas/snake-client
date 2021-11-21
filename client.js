5c223a6407077c4a5aea41de12a35fa9b5a38f76
Briana Endanawas <brianamay48@gmail.com>
2021-11-20 23:57:17 -0500

    used constants

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: '192.168.0.138',
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
   // interpret incoming data as text
@@ -12,16 +13,11 @@ const connect = function () {
   conn.on('data', (data) => {
     console.log('Message from client: ', data)
     console.log('Successfully connected to game server')
-  });
+  }); 
 
   conn.on('connect', () => {
     conn.write('Name: KTH');
-    conn.write('Move: up');
-    //setTimeout(function(){ conn.write("Move: up"); }, 50);
-    //setTimeout(function(){ conn.write("Move: down"); }, 100);
-    //setTimeout(function(){ conn.write("Move: left"); }, 150);
-    //setTimeout(function(){ conn.write("Move: right"); }, 200);
-    //setInterval(function(){ conn.write("Move: up"); }, 50);
+    //conn.write('Move: up');
   });
 
   return conn;
