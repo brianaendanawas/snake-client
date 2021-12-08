daae848fe26de2aed8c5091da3d7fa99d9090598
Briana Endanawas <brianamay48@gmail.com>
2021-12-07 21:12:09 -0500

    added message on client side to show connection to server

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const net = require("net");
 const { IP, PORT } = require("./constants");
 
+// establishes connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: IP,
@@ -10,12 +11,15 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  // receive messages from the server 
   conn.on('data', (data) => {
     console.log('Message from client: ', data);
     console.log('Successfully connected to game server');
   }); 
 
+  // send name to game server and move up once
   conn.on('connect', () => {
+    console.log('Successfully connected to game server');
     conn.write('Name: KTH');
     setTimeout(function(){ conn.write('Move: up'); }, 50);
   });
